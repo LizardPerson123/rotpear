2a195b1747a566171e093ae2e7a239395b055b83
LizardPerson123 <dylanerck@1203landmark.com>
2025-12-09 00:19:43 -0600

    Add files via upload

--- eb/httphandler.js
+++ eb/httphandler.js
@@ -1,14 +1,13 @@
 async function GetText() { 
-  let res = await fetch(`https://api.rottingpears.com/text`)
+  let res = await fetch(`/api/eb/text`)
   if (res.status != 200) {throw new Error("Error fetching text")}
   return res.text()
 }
 
-async function getLeaderboard(v) {return fetch(`https://api.rottingpears.com/leaderboard/?v=${v}`).then(res => res.json())}
-async function isScore(v, username) {return fetch(`https://api.rottingpears.com/isScore?v=${v}&username=${username}`).then(res => res.text())}
-async function submitScore(username, password, score) {return fetch('https://api.rottingpears.com/submitScore', {method:'POST', body: JSON.stringify({username: username, password: password, score: score}), headers: {"Content-Type": "application/json"}})}
+async function getLeaderboard(v) {return fetch(`/api/eb/leaderboard/?v=${v}`).then(res => res.json())}
+async function isScore(v, username) {return fetch(`/api/eb/isScore?v=${v}&username=${username}`).then(res => res.text())}
+async function submitScore(username, password, score) {return fetch('/api/eb/submitScore', {method:'POST', body: JSON.stringify({username: username, password: password, score: score}), headers: {"Content-Type": "application/json"}})}
 async function createAccount(username, password) {
-  let res = await fetch('https://api.rottingpears.com/createAccount', {method: 'POST', body: JSON.stringify({username: username, password: password}), headers: {"Content-Type": "application/json"}})
+  let res = await fetch('/api/createAccount', {method: 'POST', body: JSON.stringify({username: username, password: password}), headers: {"Content-Type": "application/json"}})
   return res.status
-
 }
