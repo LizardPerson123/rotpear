e8764b6ad5c34fb826d8c3c66388331e691202a1
LizardPerson123 <dylanerck@1203landmark.com>
2025-12-10 19:25:54 -0600

    Add files via upload

---
+++ utility.js
@@ -0,0 +1,3 @@
+function getRndInteger(min, max) {return Math.floor(Math.random() * (max - min) ) + min}
+let getById = id => {return document.getElementById(id) }
+let getByClss = className => {return Array.from(document.getElementsByClassName(className))}
