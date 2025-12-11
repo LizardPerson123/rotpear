1833a0cf0488b0677a84a23a5423e130cdddf18e
LizardPerson123 <dylanerck@1203landmark.com>
2025-12-10 19:19:59 -0600

    Add files via upload

--- eb/httphandler.js
+++ eb/httphandler.js
@@ -7,7 +7,9 @@ async function GetText() {
 async function getLeaderboard(v) {return fetch(`https://api.rottingpears.com/eb/leaderboard/?v=${v}`).then(res => res.json())}
 async function isScore(v, username) {return fetch(`https://api.rottingpears.com/eb/isScore?v=${v}&username=${username}`).then(res => res.text())}
 async function submitScore(username, password, score) {return fetch('https://api.rottingpears.com/eb/submitScore', {method:'POST', body: JSON.stringify({username: username, password: password, score: score}), headers: {"Content-Type": "application/json"}})}
+async function isAccount(username) {return fetch(`https://api.rottingpears.com/isAccount?username=${username}`).then(res => res.text())}
+
 async function createAccount(username, password) {
-  let res = await fetch('https://api.rottingpears.com/eb/text', {method: 'POST', body: JSON.stringify({username: username, password: password}), headers: {"Content-Type": "application/json"}})
+  let res = await fetch('https://api.rottingpears.com/createAccount', {method: 'POST', body: JSON.stringify({username: username, password: password}), headers: {"Content-Type": "application/json"}})
   return res.status
 }
